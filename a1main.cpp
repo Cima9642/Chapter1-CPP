7f3f4ab45be8a94861bac210c438df17069df4b4
Carlos Martinez <guitardude9642@gmail.com>
2022-12-17 15:02:39 -0600

    first commit

---
+++ a1main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+/*
+	Program that prints my name, address with city, state, and Zip code, telephone number and college major
+*/
+
+int main () {
+	
+	cout << "Name: Carlos Martinez\nAddress: 12803 West Ave, San Antonio, Tx 78216\nPhone Number: (956)435-6091\nMajor: Computer Science"<< endl;
+	
+	
+	return 0;
+}
